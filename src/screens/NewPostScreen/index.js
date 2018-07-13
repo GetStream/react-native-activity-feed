0832a1b4129f186f118cf64607386c267bea1105
Jaap <jaap@getstream.io>
2018-07-13 12:21:20 +0200

    updated header and added goBack() navigation

--- src/screens/NewPostScreen/index.js
+++ src/screens/NewPostScreen/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 class NewPostScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: "NEW POST",
+    headerLeft:
+      <TouchableOpacity
+        style={{ paddingLeft: 15 }}
+        onPress={() => navigation.goBack()}>
+        <Image
+          style={{width: 24, height: 24}}
+          source={require('../../images/icons/close.png')} />
+      </TouchableOpacity>,
+    headerRight:
+      <TouchableOpacity style={{ paddingRight: 15}}>
+        <Text style={{ color: '#007AFF', fontSize: 17}}>Send</Text>
+      </TouchableOpacity>,
+    headerTitleStyle: {
+      fontWeight: "500",
+      fontSize: 13
+    }
+  });
+
   render() {
-    return(
-      <View>
+    return (
+      <View style={styles.screenContainer}>
         <Text>NewPostScreen</Text>,
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: 'white'
+  }
+})
 
 export default NewPostScreen;
