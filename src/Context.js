ce522876f8e6b5c9663151a7fe4cc9a7f7c68848
Jelte Fennema <github-tech@jeltef.nl>
2018-07-19 11:04:12 +0200

    Profile: Move getOrCreate to Context.js

--- src/Context.js
+++ src/Context.js
@@ -64,6 +64,22 @@ export class StreamApp extends React.Component<
     };
   }
 
+  async componentDidMount() {
+    // TODO: Change this to an empty object by default
+    // TODO: Maybe move this somewhere else
+    await this.state.user.getOrCreate({
+      name: 'Batman',
+      url: 'batsignal.com',
+      desc: 'Smart, violent and brutally tough solutions to crime.',
+      profileImage:
+        'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
+      coverImage:
+        'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
+    });
+
+    this.state.changedUserData();
+  }
+
   render() {
     return (
       <StreamContext.Provider value={{ ...this.state }}>
--- src/components/ProfileHeader/index.js
+++ src/components/ProfileHeader/index.js
@@ -28,17 +28,6 @@ class ProfileHeader extends React.Component<Props, State> {
   }
 
   async componentDidMount() {
-    // TODO: Move this getOrCreate some place else, probably to app
-    // initialization so only the profile request is needed
-    await this.props.user.getOrCreate({
-      name: 'Batman',
-      url: 'batsignal.com',
-      desc: 'Smart, violent and brutally tough solutions to crime.',
-      profileImage:
-        'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
-      coverImage:
-        'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
-    });
     let data = await this.props.user.profile();
     this.props.changedUserData();
     this.setState({ user: data });
