a5071def96f75853608467b6083eb7a3b186f04f
Jaap <jaap@getstream.io>
2018-08-08 17:53:18 +0200

    updates to ogblock and statusupdateform

--- src/components/OgBlock.js
+++ src/components/OgBlock.js
@@ -1,33 +1,46 @@
 import React from 'react';
-import { Image, View, Text } from 'react-native';
+import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import _ from 'lodash';
 
 export default class OgBlock extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}>
-          {this.props.og && this.props.og.images ? (
+      <View style={[styles.wrapper, this.props.styles.wrapper]}>
+        <View style={[styles.leftColumn, this.props.styles.leftColumn]}>
+          { this.props.og && this.props.og.images ? (
+            <TouchableOpacity
+              onPress={this.props.removeOgBlock}>
+
             <Image
-              source={{
+              source={ this.props.og.images[0].image ?  {
                 uri:
-                  this.props.og.images[0].image || 'https://placehold.it/50x50',
-              }}
-              style={{ width: 50, height: 50 }}
+                  this.props.og.images[0].image,
+              } : require('../images/placeholder.png')}
+              style={[styles.image, this.props.styles.image]}
             />
-          ) : null}
+            <Image
+              source={require('../images/icons/close-black.png')}
+              style={[styles.closeButton, this.props.styles.closeButton]} />
+            </TouchableOpacity>
+          ) : null }
         </View>
-        <View style={{ flex: 1, flexDirection: 'column' }}>
-          <View style={{ width: '100%', height: 25 }}>
-            <Text>{_.truncate(this.props.og.title, { length: 100 })}</Text>
-          </View>
-          <View style={{ width: '100%', height: 25 }}>
-            <Text>
-              {_.truncate(this.props.og.description, { length: 100 })}
+        <View style={[styles.rightColumn, this.props.styles.rightColumn]}>
+            <Text
+              style={[styles.textStyle, this.props.styles.textStyle]}>
+              {_.truncate(this.props.og.title, { length: 75 })}
             </Text>
-          </View>
         </View>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: { flex: 1, flexDirection: 'row', alignItems: 'center' },
+  leftColumn: { position: 'relative' },
+  rightColumn: { flex: 1, flexDirection: 'column', marginLeft: 8 },
+  textStyle: { fontWeight: '700' },
+  image: { width: 50, height: 50, borderRadius: 4, },
+  closeButton: { width: 24, height: 24, position: 'absolute', top: -10, right: -10 }
+});
--- src/components/StatusUpdateForm.js
+++ src/components/StatusUpdateForm.js
@@ -12,6 +12,7 @@ import KeyboardAccessory from 'react-native-sticky-keyboard-accessory';
 import { Avatar, OgBlock } from 'react-native-activity-feed';
 import _ from 'lodash';
 import Symbol from 'es6-symbol';
+import { ScrollView } from '../../node_modules/react-native-gesture-handler';
 
 const ImageState = Object.freeze({
   NO_IMAGE: Symbol('no_image'),
@@ -160,9 +161,10 @@ export default class StatusUpdateForm extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.screenContainer}>
+
         <View style={styles.newPostContainer}>
           <Avatar source="https://placehold.it/100x100" size={48} />
-          <View style={styles.textInput}>
+          <ScrollView style={styles.textInput}>
             <TextInput
               multiline
               onChangeText={(text) => {
@@ -175,33 +177,50 @@ export default class StatusUpdateForm extends React.Component {
               placeholder="Share something..."
               underlineColorAndroid="transparent"
             />
-          </View>
+          </ScrollView>
         </View>
 
-        {this.state.og ? <OgBlock og={this.state.og} /> : null}
+
 
         <View>
           <KeyboardAccessory backgroundColor="#fff">
-            {this.state.image ? (
-              <View style={styles.imageContainer}>
-                <Image
-                  source={{ uri: this.state.image }}
-                  style={
-                    this.state.imageState === ImageState.UPLOADING
-                      ? styles.image_loading
-                      : styles.image
-                  }
-                />
-                <View style={styles.imageOverlay}>
-                  <TouchableOpacity>
+            <View styles={{flexDirection: 'row'}}>
+
+
+              <View>
+                {this.state.image ? (
+                  <View style={styles.imageContainer}>
                     <Image
-                      source={require('../images/icons/close-white.png')}
-                      style={{ width: 24, height: 24 }}
+                      source={{ uri: this.state.image }}
+                      style={
+                        this.state.imageState === ImageState.UPLOADING
+                          ? styles.image_loading
+                          : styles.image
+                      }
                     />
-                  </TouchableOpacity>
-                </View>
+                    <View style={styles.imageOverlay}>
+                      <TouchableOpacity>
+                        <Image
+                          source={require('../images/icons/close-white.png')}
+                          style={{ width: 24, height: 24 }}
+                        />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                ) : null}
               </View>
-            ) : null}
+              <View>
+                {this.state.og ? <OgBlock
+                  styles={{ wrapper: { padding: 15, flex: 1 } }}
+                  og={this.state.og}
+                  removeOgBlock={() => {
+                    this.setState({
+                      og: null,
+                    })
+                  }} />: null}
+              </View>
+            </View>
+
             <View style={styles.accessory}>
               <TouchableOpacity
                 title="Pick an image from camera roll"
@@ -215,6 +234,7 @@ export default class StatusUpdateForm extends React.Component {
             </View>
           </KeyboardAccessory>
         </View>
+
       </SafeAreaView>
     );
   }
@@ -245,28 +265,31 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     position: 'relative',
-    width: 100,
-    height: 100,
+    width: 50,
+    height: 50,
+    borderRadius: 4,
     margin: 15,
   },
   imageOverlay: {
     position: 'absolute',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
-    width: 100,
-    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 4,
     padding: 8,
     backgroundColor: 'rgba(0,0,0,0.4)',
   },
   image: {
     position: 'absolute',
-    width: 100,
-    height: 100,
+    width: 50,
+    height: 50,
   },
   image_loading: {
     position: 'absolute',
-    width: 100,
-    height: 100,
+    width: 50,
+    height: 50,
+    borderRadius: 4,
     opacity: 0.5,
   },
 });
