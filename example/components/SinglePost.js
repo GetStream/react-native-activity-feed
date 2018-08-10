740e0f7d1a7e05d83f3576f6829dd7e3117feaa1
Jelte Fennema <github-tech@jeltef.nl>
2018-08-10 15:02:49 +0200

    Move comment box out of SinglePost component

--- example/components/SinglePost.js
+++ example/components/SinglePost.js
@@ -1,11 +1,9 @@
 // @flow
 import React from 'react';
-import { View, SafeAreaView, StyleSheet, TextInput } from 'react-native';
-
-import KeyboardAccessory from 'react-native-sticky-keyboard-accessory';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
 
 import Activity from '../components/Activity';
-import { FlatFeed, Avatar } from 'react-native-activity-feed';
+import { FlatFeed } from 'react-native-activity-feed';
 
 import LikesList from '../components/LikesList';
 import RepostList from '../components/RepostList';
@@ -26,51 +24,36 @@ type Props = {|
 export default class SinglePost extends React.Component<Props> {
   render() {
     return (
-      <React.Fragment>
-        <SafeAreaView style={styles.container} behaviour="height" enabled>
-          <FlatFeed
-            feedGroup={this.props.feedGroup}
-            userId={this.props.userId}
-            options={{
-              withRecentReactions: true,
-            }}
-            navigation={this.props.navigation}
-            doFeedRequest={(session, feedGroup, userId, options) => {
-              return session
-                .feed(feedGroup, userId)
-                .getActivityDetail(this.props.activity.id, options);
-            }}
-            renderActivity={(props) => (
-              <React.Fragment>
-                <Activity {...props} />
-                <CommentList reactions={props.activity.latest_reactions} />
-                <RepostList reactions={props.activity.latest_reactions} />
+      <SafeAreaView style={styles.container} behaviour="height" enabled>
+        <FlatFeed
+          feedGroup={this.props.feedGroup}
+          userId={this.props.userId}
+          options={{
+            withRecentReactions: true,
+          }}
+          navigation={this.props.navigation}
+          doFeedRequest={(session, feedGroup, userId, options) => {
+            return session
+              .feed(feedGroup, userId)
+              .getActivityDetail(this.props.activity.id, options);
+          }}
+          renderActivity={(props) => (
+            <React.Fragment>
+              <Activity {...props} />
+              <CommentList reactions={props.activity.latest_reactions} />
+              <RepostList reactions={props.activity.latest_reactions} />
 
-                <View style={styles.sectionHeader} />
-                <View style={styles.likesContainer}>
-                  <LikesList
-                    reactions={props.activity.latest_reactions}
-                    reactionKind="heart"
-                  />
-                </View>
-              </React.Fragment>
-            )}
-          />
-        </SafeAreaView>
-        <KeyboardAccessory>
-          <View style={styles.replyContainer}>
-            <Avatar
-              source="https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Batman-BenAffleck.jpg/200px-Batman-BenAffleck.jpg"
-              size={48}
-            />
-            <TextInput
-              style={styles.textInput}
-              placeholder="Share something..."
-              underlineColorAndroid="transparent"
-            />
-          </View>
-        </KeyboardAccessory>
-      </React.Fragment>
+              <View style={styles.sectionHeader} />
+              <View style={styles.likesContainer}>
+                <LikesList
+                  reactions={props.activity.latest_reactions}
+                  reactionKind="heart"
+                />
+              </View>
+            </React.Fragment>
+          )}
+        />
+      </SafeAreaView>
     );
   }
 }
--- example/screens/SinglePostScreen.js
+++ example/screens/SinglePostScreen.js
@@ -1,9 +1,12 @@
 // @flow
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 import BackButton from '../components/BackButton';
 import SinglePost from '../components/SinglePost';
+
+import { Avatar } from 'react-native-activity-feed';
+import KeyboardAccessory from 'react-native-sticky-keyboard-accessory';
 
 import type { NavigationProps } from '../types';
 
@@ -29,12 +32,46 @@ export default class SinglePostScreen extends React.Component<Props> {
     const feedGroup = navigation.getParam('feedGroup');
     const userId = navigation.getParam('userId');
     return (
-      <SinglePost
-        activity={activity}
-        feedGroup={feedGroup}
-        userId={userId}
-        navigation={this.props.navigation}
-      />
+      <React.Fragment>
+        <SinglePost
+          activity={activity}
+          feedGroup={feedGroup}
+          userId={userId}
+          navigation={this.props.navigation}
+        />
+        <KeyboardAccessory>
+          <View style={styles.replyContainer}>
+            <Avatar
+              source="https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Batman-BenAffleck.jpg/200px-Batman-BenAffleck.jpg"
+              size={48}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Share something..."
+              underlineColorAndroid="transparent"
+            />
+          </View>
+        </KeyboardAccessory>
+      </React.Fragment>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  replyContainer: {
+    height: 78,
+    shadowOffset: { width: 0, height: -3 },
+    shadowColor: 'black',
+    shadowOpacity: 0.1,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+  },
+  textInput: {
+    flex: 1,
+    marginLeft: 25,
+    fontSize: 16,
+    color: '#364047',
+  },
+});
