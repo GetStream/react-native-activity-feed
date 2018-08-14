d04f4be463cc22904495b29eea17c31958eb2aa3
Jelte Fennema <github-tech@jeltef.nl>
2018-08-14 11:01:26 +0200

    Make scripts work

--- bin/postpack.sh
+++ bin/postpack.sh
@@ -1,2 +1,3 @@
 set -ex
-sed 's="main": "./lib/index.js",="main": "./src/index.js",=g'
+sed 's="main": "./lib/index.js",="main": "./src/index.js",=g' -i.bak package.json
+rm package.json.bak
--- bin/prepack.sh
+++ bin/prepack.sh
@@ -1,6 +1,7 @@
-set -ex
+set -exu
 yarn
 yarn lint
 yarn build
 git push origin master
-sed 's="main": "./src/index.js",="main": "./lib/index.js",=g'
+sed 's="main": "./src/index.js",="main": "./lib/index.js",=g' -i.bak package.json
+rm package.json.bak
