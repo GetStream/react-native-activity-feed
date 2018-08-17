9bd71197c572f3b48aaca771da0917a5d249510a
Jelte Fennema <github-tech@jeltef.nl>
2018-08-17 16:11:36 +0200

    Basic react-native expo app

---
+++ expo-example/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StreamApp } from 'react-native-activity-feed';
+
+export default class App extends React.Component {
+  render() {
+    return <StreamApp />;
+  }
+}
