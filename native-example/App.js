def735acba6f1aad00402997ed6ddff353bb3d34
Jelte Fennema <github-tech@jeltef.nl>
2018-08-20 14:02:50 +0200

    Enable broken status update in native-example

--- native-example/App.js
+++ native-example/App.js
@@ -1,18 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
+
+import KeyboardAccessory from 'react-native-sticky-keyboard-accessory';
+
 import {
   StreamApp,
   FlatFeed,
   BaseActivity,
-  Button,
+  StatusUpdateFormSimple,
+  LikeButton,
+  FeedNotification,
 } from 'react-native-activity-feed';
 
 const App = () => {
@@ -35,96 +32,36 @@ const App = () => {
     return null;
   }
 
+  // eslint-disable-next-line no-unused-vars
   function stepOne() {
     return (
-      <StreamApp
-        apiKey={apiKey}
-        appId={appId}
-        userId="batman"
-        token={token}
-        realtimeToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6IioiLCJhY3Rpb24iOiJyZWFkIiwiZmVlZF9pZCI6Im5vdGlmaWNhdGlvbmJhdG1hbiJ9.Ztf103rqNTaOq4cr9VDPfluNDW5Q8LXE28GcQYY9mzs"
-        defaultUserData={{
-          name: 'Batman',
-          url: 'batsignal.com',
-          desc: 'Smart, violent and brutally tough solutions to crime.',
-          profileImage:
-            'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
-          coverImage:
-            'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
-        }}
-      />
+      <StreamApp apiKey={apiKey} appId={appId} userId="batman" token={token} />
     );
   }
 
+  // eslint-disable-next-line no-unused-vars
   function stepTwo() {
     return (
-      <StreamApp
-        apiKey={apiKey}
-        appId={appId}
-        userId="batman"
-        token={token}
-        realtimeToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6IioiLCJhY3Rpb24iOiJyZWFkIiwiZmVlZF9pZCI6Im5vdGlmaWNhdGlvbmJhdG1hbiJ9.Ztf103rqNTaOq4cr9VDPfluNDW5Q8LXE28GcQYY9mzs"
-        defaultUserData={{
-          name: 'Batman',
-          url: 'batsignal.com',
-          desc: 'Smart, violent and brutally tough solutions to crime.',
-          profileImage:
-            'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
-          coverImage:
-            'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
-        }}
-      >
+      <StreamApp apiKey={apiKey} appId={appId} userId="batman" token={token}>
         <FlatFeed />
       </StreamApp>
     );
   }
 
+  // eslint-disable-next-line no-unused-vars
   function stepThree() {
     return (
-      <StreamApp
-        apiKey={apiKey}
-        appId={appId}
-        userId="batman"
-        token={token}
-        realtimeToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6IioiLCJhY3Rpb24iOiJyZWFkIiwiZmVlZF9pZCI6Im5vdGlmaWNhdGlvbmJhdG1hbiJ9.Ztf103rqNTaOq4cr9VDPfluNDW5Q8LXE28GcQYY9mzs"
-        defaultUserData={{
-          name: 'Batman',
-          url: 'batsignal.com',
-          desc: 'Smart, violent and brutally tough solutions to crime.',
-          profileImage:
-            'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
-          coverImage:
-            'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
-        }}
-      >
+      <StreamApp apiKey={apiKey} appId={appId} userId="batman" token={token}>
         <FlatFeed
           renderActivity={(props) => {
             return (
               <BaseActivity
                 {...props}
                 Footer={
-                  <View style={{ paddingLeft: 15 }}>
-                    <Button
-                      count={props.activity.reaction_counts.heart}
-                      on={
-                        props.activity.own_reactions.heart &&
-                        Boolean(props.activity.own_reactions.heart.length)
-                      }
-                      onPress={() =>
-                        props.onToggleReaction('heart', props.activity)
-                      }
-                      icon={{
-                        //$FlowFixMe
-                        on: require('../example/images/icons/heart.png'),
-                        //$FlowFixMe
-                        off: require('../example/images/icons/heart-outline.png'),
-                      }}
-                      label={{
-                        single: 'like',
-                        plural: 'likes',
-                      }}
-                    />
-                  </View>
+                  <LikeButton
+                    activity={props.activity}
+                    onToggleReaction={props.onToggleReaction}
+                  />
                 }
               />
             );
@@ -134,12 +71,86 @@ const App = () => {
     );
   }
 
+  // eslint-disable-next-line no-unused-vars
+  function stepFour() {
+    return (
+      <StreamApp apiKey={apiKey} appId={appId} userId="batman" token={token}>
+        <FlatFeed
+          renderActivity={(props) => {
+            return (
+              <BaseActivity
+                {...props}
+                Footer={
+                  <View style={{ paddingLeft: 15 }}>
+                    <LikeButton
+                      activity={props.activity}
+                      onToggleReaction={props.onToggleReaction}
+                    />
+                  </View>
+                }
+              />
+            );
+          }}
+        />
+
+        <KeyboardAccessory>
+          <StatusUpdateFormSimple
+            styles={{
+              ogBlock: {
+                wrapper: { padding: 8, paddingLeft: 15, paddingRight: 15 },
+              },
+            }}
+          />
+        </KeyboardAccessory>
+      </StreamApp>
+    );
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function stepFive() {
+    const renderActivity = (props) => {
+      return (
+        <BaseActivity
+          {...props}
+          Footer={
+            <View style={{ paddingLeft: 15 }}>
+              <LikeButton
+                activity={props.activity}
+                onToggleReaction={props.onToggleReaction}
+              />
+            </View>
+          }
+        />
+      );
+    };
+
+    return (
+      <StreamApp apiKey={apiKey} appId={appId} userId="batman" token={token}>
+        <FeedNotification />
+
+        <FlatFeed renderActivity={renderActivity} />
+
+        <KeyboardAccessory>
+          <StatusUpdateFormSimple
+            styles={{
+              ogBlock: {
+                wrapper: { padding: 8, paddingLeft: 15, paddingRight: 15 },
+              },
+            }}
+          />
+        </KeyboardAccessory>
+      </StreamApp>
+    );
+  }
+
   return (
-    <React.Fragment>
-      {stepOne()}
-      {stepTwo()}
-      {stepThree()}
-    </React.Fragment>
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* {stepOne()} */}
+      {/* {stepTwo()} */}
+      {/* {stepThree()} */}
+      {stepFour()}
+      {/* {stepFive()} */}
+    </SafeAreaView>
   );
 };
 
---
+++ native-package/src/index.js
@@ -0,0 +1,4 @@
+import { registerNativeHandlers } from 'react-native-activity-feed-core';
+registerNativeHandlers({});
+
+export * from 'react-native-activity-feed-core';
