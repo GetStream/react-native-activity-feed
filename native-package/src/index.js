c8c9edc29c3fba3527c5c185a80085b39ad65113
Jelte Fennema <github-tech@jeltef.nl>
2018-08-20 14:55:10 +0200

    react-native link react-native-image-picker

--- native-package/src/index.js
+++ native-package/src/index.js
@@ -1,4 +1,20 @@
 import { registerNativeHandlers } from 'react-native-activity-feed-core';
-registerNativeHandlers({});
+import ImagePicker from 'react-native-image-picker';
+registerNativeHandlers({
+  pickImage: async () => {
+    return new Promise((resolve, reject) => {
+      ImagePicker.launchImageLibrary(null, (response) => {
+        if (response.error) {
+          reject(Error(response.error));
+        }
+
+        resolve({
+          cancelled: response.didCancel,
+          uri: response.uri,
+        });
+      });
+    });
+  },
+});
 
 export * from 'react-native-activity-feed-core';
