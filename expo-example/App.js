bfba0d2e9ebdc0fe8cb3d1fdc9a3c006bb59c8ca
Jelte Fennema <github-tech@jeltef.nl>
2018-08-20 12:23:18 +0200

    Remove expo-example directory

--- expo-example/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StreamApp } from 'react-native-activity-feed';
-
-export default class App extends React.Component {
-  render() {
-    return <StreamApp />;
-  }
-}
