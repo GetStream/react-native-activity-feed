fcd61c5a232045dc10ddc6a953a2256fb70e17a8
Tommaso Barbugli <tbarbugli@gmail.com>
2018-08-28 15:12:45 +0200

    mode sed-ism

--- bin/postpack.sh
+++ bin/postpack.sh
@@ -1,3 +1,3 @@
 set -exu
-sed 's="main": "./lib/index.js",="main": "./src/index.js",=g' -i.bak package.json
+sed -e 's="main": "./lib/index.js",="main": "./src/index.js",=g' -i .bak package.json
 rm package.json.bak
