7324345d69bafef3b8af154bd1ae5fde48f12aff
Tommaso Barbugli <tbarbugli@gmail.com>
2018-08-28 15:08:04 +0200

    BSD sed is made in hell

--- bin/release.sh
+++ bin/release.sh
@@ -10,11 +10,11 @@ fi
 
 cd native-package
 npm version --no-git-tag-version "$1"
-sed 's|"react-native-activity-feed-core": "[^"]\+"|"react-native-activity-feed-core": "'"$1"'"|g' -i '.bak' package.json
+sed -e 's|"react-native-activity-feed-core": "[^"]\+"|"react-native-activity-feed-core": "'"$1"'"|g' -i .bak package.json
 rm package.json.bak
 cd ../expo-package
 npm version --no-git-tag-version "$1"
-sed 's|"react-native-activity-feed-core": "[^"]\+"|"react-native-activity-feed-core": "'"$1"'"|g' -i '.bak' package.json
+sed -e 's|"react-native-activity-feed-core": "[^"]\+"|"react-native-activity-feed-core": "'"$1"'"|g' -i .bak package.json
 rm package.json.bak
 cd ..
 git add {expo,native}-package/package.json
