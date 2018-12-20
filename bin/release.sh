61855e67b9322e9b3eb0d2625058f7f4cbc8a898
Jelte Fennema <github-tech@jeltef.nl>
2018-12-20 13:56:20 +0100

    Publish as beta

--- bin/release.sh
+++ bin/release.sh
@@ -25,12 +25,12 @@ git add native-package/yarn.lock
 npm version "$1" --force
 
 
-npm publish
+npm publish --tag beta
 
 cd native-package
-npm publish
+npm publish --tag beta
 
 cd ../expo-package
-npm publish
+npm publish --tag beta
 
 git push --follow-tags
