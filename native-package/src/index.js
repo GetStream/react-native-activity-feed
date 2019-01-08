c17d3a240d639d4dc3bdb18cc369b15633169cb7
Jelte Fennema <github-tech@jeltef.nl>
2019-01-08 10:35:41 +0100

    StatusUpdateForm: Fix image uploads for react-native ios

--- native-package/src/index.js
+++ native-package/src/index.js
@@ -1,5 +1,6 @@
 import { registerNativeHandlers } from 'react-native-activity-feed-core';
 import ImagePicker from 'react-native-image-picker';
+import { Platform } from 'react-native';
 registerNativeHandlers({
   pickImage: async () => {
     return new Promise((resolve, reject) => {
@@ -7,10 +8,14 @@ registerNativeHandlers({
         if (response.error) {
           reject(Error(response.error));
         }
+        let { uri } = response;
+        if (Platform.OS === 'android') {
+          uri = 'file://' + response.path;
+        }
 
         resolve({
           cancelled: response.didCancel,
-          uri: 'file://' + response.path,
+          uri,
         });
       });
     });
