284a20cab917e118f5193332cc2b47a67e446c65
Vishal Narkhede <vishal@getstream.io>
2019-08-19 12:10:57 +0200

    Fixing the example config

--- examples/one/App.js
+++ examples/one/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
-import { StreamApp, FlatFeed } from 'react-native-activity-feed';
+import {
+  StreamApp,
+  FlatFeed,
+  Activity,
+  LikeButton,
+  StatusUpdateForm,
+} from 'react-native-activity-feed';
+
+const CustomActivity = props => {
+  return <Activity {...props} Footer={<LikeButton {...props} />} />;
+};
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}} forceInset={{ top: 'always' }}>
+    <SafeAreaView style={{flex: 1}} forceInset={{top: 'always'}}>
       <StreamApp
-          apiKey="5rqsbgqvqphs"
-          appId="40273"
-          token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZTI0MWU0ZWUtYjQ0OC00YWU5LTg0NTItYjFmMzA3ZjY2YmI3In0.tnV09TmKuQXfXBTdrQeECcqluJ4BuGHEw9YoWJIihUk"
-      >
-        <FlatFeed />
+        apiKey="5rqsbgqvqphs"
+        appId="40273"
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZTI0MWU0ZWUtYjQ0OC00YWU5LTg0NTItYjFmMzA3ZjY2YmI3In0.tnV09TmKuQXfXBTdrQeECcqluJ4BuGHEw9YoWJIihUk">
+        <FlatFeed Activity={CustomActivity} />
+        <StatusUpdateForm feedGroup="timeline" />
       </StreamApp>
     </SafeAreaView>
   );
