01e19d0dbc34d32b0365e93dcd25c2260adb545f
Jaap <jaap@getstream.io>
2019-08-20 15:39:43 +0200

    update example app

--- examples/one/App.js
+++ examples/one/App.js
@@ -1,126 +1,177 @@
 import React from 'react';
-import SafeAreaView from 'react-native-safe-area-view';
-import {ScrollView, View} from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity, SafeAreaView } from 'react-native';
 import {
+  Avatar,
   StreamApp,
   FlatFeed,
   Activity,
-  LikeButton,
   StatusUpdateForm,
+  LikeButton,
+  IconBadge,
+  SinglePost,
+  CommentBox,
   ReactionIcon,
   CommentList,
-  CommentBox
+  RepostList,
+  LikesList,
+  CommentItem
 } from 'react-native-activity-feed';
 
-const CustomActivity = props => {
-  return (
-    <Activity {...props} Footer={
-      <View style= {{ marginTop: 15 }}>
-        <View style= {{ flexDirection: 'row', alignItems: 'center' }}>
-          <LikeButton reactionKind= "like" {...props} />
-          <ReactionIcon
-            labelSingle="comment"
-            labelPlural="comments"
-            counts={props.activity.reaction_counts}
-            kind="comment"
-          />
-        </View>
-        <CommentList style={{ marginTop: 15}} activityId= {props.activity.id} />
-        <CommentBox style= {{ marginTop: 15}} noKeyboardAccessory= {true} activity={props.activity} onAddReaction= {props.onAddReaction} />
-        <View style= {{ backgroundColor: '#DCDCDC', height: 10 }} />
-      </View>
-    } />
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+const apiKey = '6hwxyxcq4rpe';
+const appId = '35808';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYmF0bWFuIn0.8MshXS9c3Du81ul3mWwuT6HH06fP45O-GvrOuJA82y4';
+
+const HomeScreen = function (props) {
+  _onPressActivity = (activity) => {
+    props.navigation.navigate('SinglePost', {
+      activity: activity,
+    });
+  };
+
+  const renderActivity = (props) => (
+    <TouchableOpacity
+            onPress={() => _onPressActivity(props.activity)}
+          >
+      <Activity {...props} Footer={<LikeButton {...props} />} />
+    </TouchableOpacity>
   );
-};
 
-const apiKey = '5rqsbgqvqphs'
-const appId = '40273'
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZTI0MWU0ZWUtYjQ0OC00YWU5LTg0NTItYjFmMzA3ZjY2YmI3In0.tnV09TmKuQXfXBTdrQeECcqluJ4BuGHEw9YoWJIihUk'
-
-const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}} forceInset={{top: 'always'}}>
-      <StreamApp
-        apiKey={apiKey}
-        appId={appId}
-        token={token}>
-        <FlatFeed Activity={CustomActivity} notify />
-        <StatusUpdateForm feedGroup="timeline" />
-      </StreamApp>
-    </SafeAreaView>
-  );
-};
-
-
-
-
-const App2 = () => {
-  return (
-    <SafeAreaView style= {{ flex: 1}} forceInset= {{ top: 'always' }}>
-      {/* <ScrollView style= {{ flex: 1}}> */}
-        <StreamApp
-          apiKey={apiKey}
-          appId={appId}
-          token={token}>
-          <StatusUpdateForm noKeyboardAccessory={true} feedGroup= "timeline"
-            // onSuccess= {() => {
-            //  this.onPostCreate()
-            // }}
-          />
-          <FlatFeed
-            notify
-            style= {{ alignItems: 'stretch' }}
-            Activity= {( data) => (
-              <Activity
-                {...data}
-                // onPress= {() => {
-                //   this.props.navigation.navigate('SinglePost', {
-                //     activityId: data.activity.id,
-                //     user_id: this.props.auth.userId,
-                //     apiKey: this.state.apiKey,
-                //     appId: this.state.appId,
-                //     token: this.state.token
-                //   })}
-                // }
-                style={{  justifyContent: 'space-between' }}
-                Footer={
-                  <View style={{ marginTop: 15 }}>
-                    <View style={{
-                      flexDirection: 'row',
-                      alignItems: 'center'
-                    }}>
-                      <LikeButton reactionKind= "like" {...data} />
-                      <ReactionIcon
-                        labelSingle="comment"
-                        labelPlural="comments"
-                        counts={data.activity. reaction_counts}
-                        kind="comment"
-                      />
-                    </View>
-                    <CommentList
-                      style={{ marginTop: 15}}
-                      activityId={data.activity.id}
-                    />
-                    <CommentBox
-                      style= {{ marginTop: 15}}
-                      noKeyboardAccessory={true}
-                      activity={data.activity}
-                      onAddReaction={data.onAddReaction}
-                    />
-                    <View style={{
-                      backgroundColor: '#DCDCDC',
-                      height: 10
-                    }} />
-                  </View>
-                }
-              />
-            )}
-          />
-        </StreamApp>
-      {/* </ScrollView> */}
-    </ SafeAreaView>
+    <StreamApp
+      apiKey={apiKey}
+      appId={appId}
+      token={token}
+      userId='batman'
+      defaultUserData={{
+        name: 'Batman',
+        url: 'batsignal.com',
+        desc: 'Smart, violent and brutally tough solutions to crime.',
+        profileImage:
+          'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
+        coverImage:
+          'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
+      }}
+    >
+       <FlatFeed Activity={renderActivity} notify />
+       <StatusUpdateForm feedGroup="timeline" />
+    </StreamApp>
   );
 }
 
+class SinglePostScreen extends React.Component<Props> {
+  static navigationOptions = ({ navigation }: Props) => ({
+    title: 'POST DETAIL',
+  });
 
-export default App2;
+  render() {
+    const { navigation } = this.props;
+    const activity = navigation.getParam('activity');
+    const feedGroup = navigation.getParam('feedGroup');
+    const userId = navigation.getParam('userId');
+    console.log(activity)
+    return (
+       <StreamApp
+        apiKey={'6hwxyxcq4rpe'}
+        appId={'35808'}
+        token={'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYmF0bWFuIn0.8MshXS9c3Du81ul3mWwuT6HH06fP45O-GvrOuJA82y4'}
+        defaultUserData={{
+          name: 'Batman',
+          url: 'batsignal.com',
+          desc: 'Smart, violent and brutally tough solutions to crime.',
+          profileImage:
+            'https://i.kinja-img.com/gawker-media/image/upload/s--PUQWGzrn--/c_scale,f_auto,fl_progressive,q_80,w_800/yktaqmkm7ninzswgkirs.jpg',
+          coverImage:
+            'https://i0.wp.com/photos.smugmug.com/Portfolio/Full/i-mwrhZK2/0/ea7f1268/X2/GothamCity-X2.jpg?resize=1280%2C743&ssl=1',
+        }}
+      >
+          <SinglePost
+            activity={activity}
+            feedGroup={feedGroup}
+            options={{
+              withOwnChildren: true
+            }}
+            userId={userId}
+            navigation={this.props.navigation}
+            Activity={(props) => (
+              <React.Fragment>
+                <Activity
+                  {...props}
+                  Footer={
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <LikeButton {...props} />
+
+                      <ReactionIcon
+                        labelSingle="comment"
+                        labelPlural="comments"
+                        counts={props.activity.reaction_counts}
+                        kind="comment"
+                      />
+                    </View>
+                  }
+                />
+                <CommentList
+                  CommentItem={({ comment }) => (
+                    <React.Fragment>
+                      <CommentItem
+                        comment={comment}
+                        Footer={<LikeButton reaction={comment} {...props} />}
+                      />
+                    </React.Fragment>
+                  )}
+                  activityId={props.activity.id}
+                  reactions={props.activity.latest_reactions} />
+                {/* <RepostList reactions={props.activity.latest_reactions} /> */}
+
+                {/* <View style={styles.sectionHeader} />
+                <View style={styles.likesContainer}>
+                  <LikesList
+                    activityId={props.activity.id}
+                    reactions={props.activity.latest_reactions}
+                    reactionKind="heart"
+                  />
+                </View> */}
+              </React.Fragment>
+            )}
+            Footer={(props) => {
+              return (
+                <CommentBox
+                  onSubmit={(text) =>
+                    props.onAddReaction('comment', activity, { text: text })
+                  }
+                  avatarProps={{
+                    source: (userData: UserResponse) =>
+                      userData.data.profileImage,
+                  }}
+                  styles={{ container: { height: 78 } }}
+                />
+              );
+            }}
+          />
+      </StreamApp>
+
+    );
+  }
+}
+
+
+
+const Navigation = createStackNavigator({
+  Home: HomeScreen,
+  SinglePost: SinglePostScreen,
+});
+
+const AppContainer = createAppContainer(Navigation);
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AppContainer
