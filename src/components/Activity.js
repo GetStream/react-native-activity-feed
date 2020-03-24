724db3fa8b460173d3313dcdd6ec36bd5a33ecbf
Saulo Vallory <saulo@toki.life>
2020-03-23 23:05:32 -0300

    Prettier formatting

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -182,7 +182,11 @@ export default class Activity extends React.Component<Props> {
           </Text>,
         );
       } else {
-        rendered.push(<Text key={i} style={styles.text}>{tokens[i] + ' '}</Text>);
+        rendered.push(
+          <Text key={i} style={styles.text}>
+            {tokens[i] + ' '}
+          </Text>,
+        );
       }
     }
     return rendered;
